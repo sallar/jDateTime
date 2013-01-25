7654a6c4f9c1d5e704b4254842a40253a35b10c5
Afshin Mehrabani <afshin.meh@gmail.com>
2013-01-25 12:33:53 +0330

    Remove old redundant test + add setUp to new test

--- Tests/HelloWorldTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class HelloWorldTest extends PHPUnit_Framework_TestCase
-{
-    public function testHelloWorld()
-    {
-        $this->assertEquals('Hello World', 'Hello World');
-    }
-}
--- Tests/JDateTimeTest.php
+++ Tests/JDateTimeTest.php
@@ -2,6 +2,17 @@
 
 class JDateTimeTest extends PHPUnit_Framework_TestCase
 {
+		//Start test
+		public function setUp()
+    {
+        //nothing
+    }
+    //EOF
+    public function tearDown()
+    {
+        //nothing
+    }
+
     public function testFirstTest()
     {
         //It works!
