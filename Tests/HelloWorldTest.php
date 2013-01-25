759a316d4da93cc00e5fecc2f00deeae7941922a
Afshin Mehrabani <afshin.meh@gmail.com>
2013-01-25 12:29:03 +0330

    Fix test

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -4,8 +4,6 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
 {
     public function testHelloWorld()
     {
-        $helloWorld = new HelloWorld($this->pdo);
-
-        $this->assertEquals('Hello World', $helloWorld->hello());
+        $this->assertEquals('Hello World', 'Hello World');
     }
 }
