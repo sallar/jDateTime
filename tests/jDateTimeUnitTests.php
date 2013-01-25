d26a41a6f6fc55a5d5461b5176f799c7cf3c0f83
Afshin Mehrabani <afshin.meh@gmail.com>
2013-01-25 11:50:42 +0330

    Add new test class

---
+++ tests/jDateTimeUnitTests.php
@@ -0,0 +1,22 @@
+<?php
+
+class jDateTimeUnitTests extends PHPUnit_Framework_TestCase
+{
+    //Start testing
+    public function setUp()
+    {
+        //nothing
+    }
+
+    //EOF
+    public function tearDown()
+    {
+        //nothing
+    }
+
+    public function testHelloWorld()
+    {
+        //It works!
+        $this->assertEquals(1, 1);
+    }
+}
