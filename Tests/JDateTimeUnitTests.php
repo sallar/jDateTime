7e3f4432ca5d096f13f5b960eb0a918a6f5f37a5
Afshin Mehrabani <afshin.meh@gmail.com>
2013-01-25 12:17:56 +0330

    Add fixed test

---
+++ Tests/JDateTimeUnitTests.php
@@ -0,0 +1,22 @@
+<?php
+
+class JDateTimeUnitTests extends PHPUnit_Framework_TestCase
+{
+    //Start testing
+    public function setUp()
+    {
+        //nothing
+    }
+
+    //EOF
+    public function tearDown()
+    {
+        //nothing
+    }
+
+    public function testWorking()
+    {
+        //It works!
+        $this->assertEquals(1, 1);
+    }
+}
