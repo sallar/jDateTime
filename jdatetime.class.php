0ac89a76078b896df061fa7f2e539a6b0c54ce9c
Meysam <p.g.meysam@gmail.com>
2016-03-06 09:56:32 +0330

    Fix getdate() codes based on code style of project.

--- jdatetime.class.php
+++ jdatetime.class.php
@@ -414,49 +414,49 @@ class jDateTime
     /**
      * jDateTime::getdate
      *
-     * Like php built-in function, returns an associative array containing the date information of a timestamp.
+     * Like php built-in function, returns an associative array containing the date information of a timestamp, or the current local time if no timestamp is given. .
      *
      * @author Meysam Pour Ganji
      * @param $timestamp int The timestamp that whould convert to date information array, if NULL passed, current timestamp will be processed.
      * @return An associative array of information related to the timestamp. For see elements of the returned associative array see {@link http://php.net/manual/en/function.getdate.php#refsect1-function.getdate-returnvalues}.
      */
-    public static function getdate( $timestamp = NULL )
+    public static function getdate($timestamp = null)
     {
-    	if( $timestamp === NULL )
-    	{
-    		$timestamp = time();
-    	}
-    	
-       	if( is_string($timestamp) )
-    	{
-    		if( ctype_digit($timestamp) || ( $timestamp{0} == '-' && ctype_digit(substr($timestamp, 1)) ) )
-    		{
-    			$timestamp = (int)$timestamp;
-    		}
-    		else
-    		{
-    			$timestamp = strtotime($timestamp);
-    		}
-    	}
-    	
-    	$dateString = self::date("s|i|G|j|w|n|Y|z|l|F", $timestamp);
-    	$dateArray = explode("|", $dateString);
-    	
-    	$result = array(
-    			"seconds" => $dateArray[0],
-    			"minutes" => $dateArray[1],
-    			"hours" => $dateArray[2],
-    			"mday" => $dateArray[3],
-    			"wday" => $dateArray[4],
-    			"mon" => $dateArray[5],
-    			"year" => $dateArray[6],
-    			"yday" => $dateArray[7],
-    			"weekday" => $dateArray[8],
-    			"month" => $dateArray[9],
-    			0 => $timestamp
-    		); 
-    	
-    	return $result;
+        if ( $timestamp === null )
+        {
+            $timestamp = time();
+        }
+        
+        if ( is_string($timestamp) )
+        {
+            if( ctype_digit($timestamp) || ( $timestamp{0} == '-' && ctype_digit(substr($timestamp, 1)) ) )
+            {
+                $timestamp = (int)$timestamp;
+            }
+            else
+            {
+                $timestamp = strtotime($timestamp);
+            }
+        }
+        
+        $dateString = self::date("s|i|G|j|w|n|Y|z|l|F", $timestamp);
+        $dateArray = explode("|", $dateString);
+        
+        $result = array(
+            "seconds" => $dateArray[0],
+            "minutes" => $dateArray[1],
+            "hours" => $dateArray[2],
+            "mday" => $dateArray[3],
+            "wday" => $dateArray[4],
+            "mon" => $dateArray[5],
+            "year" => $dateArray[6],
+            "yday" => $dateArray[7],
+            "weekday" => $dateArray[8],
+            "month" => $dateArray[9],
+            0 => $timestamp
+        ); 
+        
+        return $result;
     }
 
     /**
